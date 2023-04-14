a0468ec7cafbc71ddb363f7de3a9a9af824327fc
Alan Dono <alannicolasdono@gmail.com>
2023-04-14 16:04:08 -0300

    proyecto terminado

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -4,7 +4,7 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 
 function Tarea ({ id, texto, completada, completarTarea, eliminarTarea }) {
   return (
-    <div className={ completada ? 'tarea-contenedor completada' : 'tarea-contenedor' }>
+    <div className={ completada ? 'completada' : 'tarea-contenedor' }>
       <div 
         className='tarea-texto'
         onClick={() => completarTarea(id)} >
